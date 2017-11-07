c92f1722bdd4567730b231680b10740afdc11500
Jon Gaspar <jon_gaspar@hotmail.com>
2017-11-07 21:11:09 +0000

    Completed exercise 1

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
+  attr_reader :name, :annual_revenue, :mens_apparel, :womens_apparel
 
+  def initializer(name, annual_revenue, mens_apparel, womens_apparel)
+    @name = name
+    @annual_revenue = annual_revenue
+    @mens_apparel = mens_apparel
+    @womens_apparel = womens_apparel
+  end
 end
